1f4fa67af3aa595f15763b12fe329094f330ed1f
Emeric Lesage <elesage.dev@gmail.com>
2024-10-15 17:16:04 +0200

    update json, try added every card with JS, litle modif on HTML

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-// const case1 = document.getElementsByClassName("case1");
-// case1.classList.add()
-// case1.append()
+const flip = document.querySelector(".cardBack");
+flip.addEventListener("click", () => {
+	alert("Check");
+});
+
+/////////////////////////////////
+/////////////////////////////////
+/////////////////////////////////
+const cardBack = document.querySelector(".cardBack");
+
+const campus = [
+	{
+		name: "Emeric L",
+		bootcamp: "Dev",
+		project: "Poursuite en alternance",
+		hobbies: ["Jeux vidéo", "Cinéma", "Sport"],
+		quote: "Pour chaque fin, il y a toujours un nouveau départ",
+		githublink: "https://github.com/lesage-emeric",
+		linkedin: "www.linkedin.com/in/emeric-lesage-610934285",
+		imgpro: "photos-pro/imgProEmericL.jpeg",
+		imgfun: "photos-fun/imgFunEmericL.jpg",
+	},
+	{
+		name: "Louis V",
+		bootcamp: "Dev",
+		project: "Poursuite en alternance",
+		hobbies: ["E-commmerce", "Business", "Sport"],
+		quote: "",
+		githublink: "https://github.com/LouisNoizar",
+		linkedin: "",
+		imgpro: "",
+		imgfun: "",
+	},
+];
+
+function createCard(people) {
+	const { name, /* bootcamp, */ imgpro } = people;
+
+	const card = document.createElement("cardBack");
+	card.classList.add("cardBack");
+	cardBack.append(card);
+
+	const cardTitle = document.createElement("h6");
+	cardTitle.textContent = name;
+	cardBack.append(cardTitle);
+
+	// const cardBoot = document.createElement("h6");
+	// cardBoot.classList.add("");
+	// cardBoot.textContent = bootcamp;
+	// cardBack.append(cardBoot);
+
+	const cardImg = document.createElement("img");
+	cardImg.src = imgpro;
+	cardImg.alt = `Photo professionnelle de ${name}`;
+	cardImg.classList.add("photoPro");
+	cardBack.append(cardImg);
+}
+for (const people of campus) {
+	createCard(people);
+}
